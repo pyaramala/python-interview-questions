c72cbe3f2baa0ee77be36bde13604cea613a462f
yaramalapradeep <pradeepyaramala1624@gmail.com>
2024-05-04 11:13:26 -0700

    questions added

--- string_datastructure.py
+++ string_datastructure.py
@@ -64,5 +64,60 @@ Enter String: hello world
 helo wrd
 '''
 
-# 6. 
+# 6. Find the first non-repeating character in a string
 
+s = input("Enter String")
+char_count_dict = {}
+for char in s:
+    char_count_dict[char]=char_count_dict.get(char,0)+1
+for char in s:
+    if char_count_dict[char]==1:
+        print(char)
+        break
+
+'''
+Enter String pradeep
+r
+'''
+
+
+# 7. Check if a string contain only digits
+
+s = input("Enter String: ")
+if s.isdigit():
+    print("String contain only digits")
+else:
+    print("NO")
+
+
+'''
+Enter String: 1234
+String contain only digits
+PS C:\Users\new\python_interview_questions\python-interview-questions> python test.py
+Enter String: p123
+NO
+'''
+
+# 8. Check if a string contains onlyy alphanumeric characters
+
+s = input("Enter String: ")
+if s.isalnum():
+    print("YES")
+else:
+    print("No")
+
+''' 
+PS C:\Users\new\python_interview_questions\python-interview-questions> python test.py
+Enter String: abc123
+YES
+PS C:\Users\new\python_interview_questions\python-interview-questions> python test.py
+Enter String: 123
+YES
+PS C:\Users\new\python_interview_questions\python-interview-questions> python test.py
+Enter String: 123!
+No
+PS C:\Users\new\python_interview_questions\python-interview-questions> 
+
+'''
+
+# 9.Remove all white spaces from a string
