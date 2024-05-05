7c3be5208589a92491681a0f647186dc4381965f
yaramalapradeep <pradeepyaramala1624@gmail.com>
2024-05-05 02:32:19 -0700

    questions updated

--- string_datastructure1.py
+++ string_datastructure1.py
@@ -52,3 +52,49 @@ def count_vowels_and_consonants(input_string):
 
 count_vowels_and_consonants("Hello World")  #3 7
 
+
+# 16. Check if a string contains palindrome substring of length greater than or equal to k
+
+s = input("Enter String: ")
+n = len(s)
+k=4
+sub_string_list = []
+for i in range(len(s)):
+    for j in range(i+1,n+1):
+        sub_string_list.append(s[i:j])
+new_list = [substr for substr in sub_string_list if len(substr)==k and substr==substr[::-1]]
+print(new_list)
+
+'''
+Enter String: abccba
+['bccb']
+'''
+
+# 17.Find the first repeated character in a string
+
+s = input("Enter String: ")
+word_count_dict = {}
+for char in s:
+    if char  in word_count_dict:
+        print(char)
+        break
+    else:
+        word_count_dict[char]=1
+
+'''
+Enter String: abcdefghija
+a
+'''
+
+# 18.Find the index of the first occurence of substring in a string
+s = input("Enter Main string: ")
+sub_s = input("Enter sub string: ")
+print(s.find(sub_s))
+
+'''
+Enter Main string: hello world
+Enter sub string: world
+6
+'''
+
+#19.
