04bb0f06f76f293d95cd7e6957c0ffe04de4e9ea
yaramalapradeep <pradeepyaramala1624@gmail.com>
2024-05-05 05:52:24 -0700

    17 questions

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-d ={'pradeep': 300, 'yaramala': 100, 'nani': 10} 
-d1 = d.copy()
-d1['pradeep']=00
-print(id(d))
+d = {'pradeep': 300, 'yaramala': 100, 'nani': 10} 
+d1 ={'city': 300, 'yaramala': 20, 'nani': 10} 
+d.update(d1)
+print(d)
